4c42bcaae6c0e929d4c1f1ff5187c6b597c28813
Thiago Bazani <tgobzrj@live.com>
2022-05-06 10:02:04 -0300

    atualizar angular

--- atividade01/src/app/app.module.ts
+++ atividade01/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http'
+
 import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './views/header/header.component';
 import { LoginComponent } from './views/login/login.component';
@@ -11,20 +12,23 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './views/footer/footer.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
     LoginComponent,
-    FooterComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FontAwesomeModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- atividade01/src/app/models/user.ts
+++ atividade01/src/app/models/user.ts
@@ -1,6 +1,6 @@
 export class User {
   constructor(
     public email: string,
-    public senha: string,
+    public senha: string
   ){}
 }
