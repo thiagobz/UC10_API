bd59f9494ff196814c1762b05814fb906af06167
Thiago Bazani <tgobzrj@live.com>
2022-05-07 15:16:28 -0300

    sistema de login implementado

--- atividade01/src/app/views/login/login.component.ts
+++ atividade01/src/app/views/login/login.component.ts
@@ -18,11 +18,41 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  userModel = new User("", "")
+  loginModel = new User()
+  mensagem = ""
+  usuarioLogado = ""
+
   
   onSubmit(){
-    console.log(this.userModel.email);
-    console.log(this.userModel.password);
+    console.log(this.loginModel);
+
+    let erroEncontrado = 0;
+    
+    const listaPalavras: string[] = ["select ", "from ", "drop ", "or ", "having ", "group ", "insert ", "exec ", "\"", "\'", "--", "#", "*", ";"]
+
+    listaPalavras.forEach(palavra =>{
+      console.log("palavra atual: ", palavra)
+
+      if (this.loginModel.email.toLowerCase().includes(palavra)) {
+        console.log("palavra encontrada: ", palavra)
+        this.mensagem = "Dados inválidos: " + palavra;
+      }
+      erroEncontrado = 1;
+    })
+
+    if(erroEncontrado == 0){
+      this.LoginService.login(this.loginModel).subscribe((response)=> {
+        this.usuarioLogado = response.body.user.nome
+        
+        // console.log(response)
+
+        console.log(this.usuarioLogado);
+
+        // this.router.navigateByUrl("")
+      }), (respostaErro: { error: string; }) => {
+        this.mensagem = respostaErro.error
+      }
+    }
   }
 
   arrowRight = faArrowRight
